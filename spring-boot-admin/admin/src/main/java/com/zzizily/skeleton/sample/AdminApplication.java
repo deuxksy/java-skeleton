45d703b400128995c089678e58ffb6a212b69ce9
SeokYoung Kim <deuxksy@gmail.com>
2018-08-28 01:02:15 +0900

    spring boot admin 추가

---
+++ spring-boot-admin/admin/src/main/java/com/zzizily/skeleton/sample/AdminApplication.java
@@ -0,0 +1,13 @@
+package com.zzizily.skeleton.sample;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class);
+    }
+}
