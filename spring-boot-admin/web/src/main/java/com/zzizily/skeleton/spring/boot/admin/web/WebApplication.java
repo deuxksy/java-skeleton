d5cd9c299515346e0fd4d76c4208c4e831309052
SeokYoung Kim <deuxksy@gmail.com>
2018-10-02 01:19:54 +0900

    단순 변수 선언 및 간단한 수정

--- spring-boot-admin/web/src/main/java/com/zzizily/skeleton/spring/boot/admin/web/WebApplication.java
+++ spring-boot-admin/web/src/main/java/com/zzizily/skeleton/spring/boot/admin/web/WebApplication.java
@@ -2,9 +2,18 @@ package com.zzizily.skeleton.spring.boot.admin.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
+
+  @RequestMapping(value="/home")
+  public String home(){
+    return "test";
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(WebApplication.class, args);
